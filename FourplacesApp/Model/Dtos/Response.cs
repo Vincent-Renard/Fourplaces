ecc06b4cc78d7cbf0e91fde6556cecbe29aafd79
Renard Vincent <vincent.renard@etu.univ-orleans.fr>
2019-03-02 14:29:44 +0100

    +Response json deserialize +correction observable list

---
+++ FourplacesApp/Model/Dtos/Response.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Model.Dtos
+{
+	public class Response
+	{
+		[JsonProperty("is_success")]
+		public bool IsSuccess { get; set; }
+		
+		[JsonProperty("error_code")]
+		public string ErrorCode { get; set; }
+		
+		[JsonProperty("error_message")]
+		public string ErrorMessage { get; set; }
+	}
+
+	public class Response<T> : Response
+	{
+		[JsonProperty("data")]
+		public T Data { get; set; }
+
+        public static explicit operator Response<T>(object v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- FourplacesApp/RestService.cs
+++ FourplacesApp/RestService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Model.Dtos;
 using Newtonsoft.Json;
@@ -12,38 +14,46 @@ namespace FourplacesApp
         private readonly String serviceURI = "https://td-api.julienmialon.com";
         private readonly String _placesURI = "/places";
         private readonly String _rootURI = "/";
+        private readonly String _loginURI = "auth/login";
+        private readonly String _loginRegisterURI = "auth/register";
+        private readonly String _loginRefreshURI = "auth/refresh";
+        private readonly String _meURI = "/me";
+        private readonly String _patchPasswordURI = "/me/password";
+        private readonly String _imagesURI = "/images";
+        private readonly String _commentsURI = "/images";//a mettre apres seviveuri/placesURI/{id}/
         private HttpClient client;
 
         public RestService()
         {
-            client = new HttpClient{ MaxResponseContentBufferSize = 256000 };
+            client = new HttpClient { MaxResponseContentBufferSize = 256000 };
 
         }
         public async void GetRoot()
         {
 
-           
 
-            var uri = new Uri(string.Format(this.serviceURI+_rootURI, string.Empty));
 
-              var response = await client.GetAsync(uri);
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine(content);
-                }
-            
-            
+            var uri = new Uri(string.Format(this.serviceURI + _rootURI, string.Empty));
+
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+
+
 
 
 
 
         }
-        public async Task<List<PlaceItem>> GetListPlacesAsync() {
-  
-            List<PlaceItem> toRet = new List<PlaceItem>();
+        public async Task<ObservableCollection<PlaceItemSummary>> GetListPlacesAsync()
+        {
 
-          var uri = new Uri(string.Format(this.serviceURI+this._placesURI, string.Empty));
+            ObservableCollection<PlaceItemSummary> toRet = new ObservableCollection<PlaceItemSummary>();
+
+            var uri = new Uri(string.Format(this.serviceURI + this._placesURI, string.Empty));
 
             try
             {
@@ -51,15 +61,31 @@ namespace FourplacesApp
                 if (response.IsSuccessStatusCode)
                 {
                     var content = await response.Content.ReadAsStringAsync();
-                    toRet = JsonConvert.DeserializeObject<List<PlaceItem>>(content);
+                    toRet = JsonConvert.DeserializeObject<Response<ObservableCollection<PlaceItemSummary>>>(content).Data; ;
                 }
             }
-            catch (Exception ex) { }
+            catch (Exception ex) { Console.WriteLine("EROORRR " + ex.Message); }
 
 
 
             return toRet;
 
         }
+        public async Task<LoginResult> Signin(RegisterRequest user)
+        {
+            Response<LoginResult> toks = null;
+            var uri = new Uri(string.Format(this.serviceURI + this._loginRegisterURI, string.Empty));
+            var json = JsonConvert.SerializeObject(user);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = null;
+            response = await client.PostAsync(uri, content);
+            if (response.IsSuccessStatusCode)
+            {
+                var rep = await response.Content.ReadAsStringAsync();
+                toks = JsonConvert.DeserializeObject<Response<LoginResult>>(rep);
+            }
+            return toks.Data;
+        }
+
     }
 }
