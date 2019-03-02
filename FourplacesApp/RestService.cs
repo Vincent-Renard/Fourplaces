914b20ddffce475a65f3ebdea5d74481c90222b5
Renard Vincent <vincent.renard@etu.univ-orleans.fr>
2019-03-02 11:13:13 +0100

    puch test

---
+++ FourplacesApp/RestService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FourplacesApp
+{
+    public class RestService
+    {
+        private readonly String getPlacesURI = "https://td-api.julienmialon.com/places";
+
+        public RestService()
+        {
+
+        }
+    }
+}
