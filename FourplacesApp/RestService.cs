d32d678d85f405020ffbd7c4c330c1135f7f4266
Renard Vincent <vincent.renard@etu.univ-orleans.fr>
2019-03-02 11:38:56 +0100

    Add GetListePlaces

--- FourplacesApp/RestService.cs
+++ FourplacesApp/RestService.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+using TD.Api.Dtos;
+using System.Console;
+
 namespace FourplacesApp
 {
     public class RestService
     {
         private readonly String getPlacesURI = "https://td-api.julienmialon.com/places";
+        private HttpClient client;
 
         public RestService()
         {
+            client = new HttpClient{ MaxResponseContentBufferSize = 256000 };
+
+        }
+        public async System.Threading.Tasks.Task<List<PlaceItem>> GetListPlacesAsync() {
+  
+            List<PlaceItem> toRet = new List<PlaceItem>();
+
+          var uri = new Uri(string.Format(this.getPlacesURI, string.Empty));
+
+            try
+            {
+                var response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    toRet = JsonConvert.DeserializeObject<List<PlaceItem>>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return toRet;
 
         }
     }
