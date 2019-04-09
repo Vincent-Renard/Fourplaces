d70447eda3373000d6e58799fd5a91c9a30b7eae
viren <vincent.renard@gmx.com>
2019-04-09 19:11:15 +0200

    * PlaceView.xaml:
    * FourplacesApp.csproj: Ajout PlaceView

---
+++ FourplacesApp/View/PlaceView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FourplacesApp.View
+{
+    public partial class PlaceView : ContentPage
+    {
+        public PlaceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FourplacesApp/ViewModel/PlaceViewViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FourplacesApp.ViewModel
+{
+    public class PaceViewViewModel : ContentPage
+    {
+        public PaceViewViewModel()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
