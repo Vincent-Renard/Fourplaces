811161d84261d2072bca92117b6d248f2fdaefe1
viren <vincent.renard@gmx.com>
2019-04-16 22:52:56 +0200

    new placeView

---
+++ FourplacesApp/View/NewPlaceView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using FourplacesApp.ViewModel;
+using Storm.Mvvm.Forms;
+using Xamarin.Forms;
+
+namespace FourplacesApp
+{
+    public partial class NewPlaceView : BaseContentPage
+    {
+        public NewPlaceView()
+        {
+            InitializeComponent();
+            BindingContext = new NewPlaceViewViewModel(Navigation);
+        }
+    }
+}
