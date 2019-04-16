23f0843eafcf10fa0b26db5f67366e5a777693b3
viren <vincent.renard@gmx.com>
2019-04-16 22:50:56 +0200

    + Localisation async

--- FourplacesApp/App.xaml.cs
+++ FourplacesApp/App.xaml.cs
@@ -4,7 +4,9 @@ using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Model;
 using Model.Dtos;
+using Plugin.Geolocator;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
@@ -12,18 +14,18 @@ namespace FourplacesApp
 {
     public partial class App : Application
     {
-
         public static RestService API;
-
         public static readonly double RadiusMap = 2.5;
+        private static Position defaultPosition;
 
         public App()
         {
-  
+
             API = new RestService();
             InitializeComponent();
             MainPage = new NavigationPage(new Menu());
-           
+            defaultPosition = new Position(47.845647, 1.939958);
+
         }
 
         protected override void OnStart()
@@ -40,5 +42,36 @@ namespace FourplacesApp
         {
             // Handle when your app resumes
         }
+
+        public static async Task<Position> LocalisationAsync()
+        {
+            try
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+                if (CrossGeolocator.IsSupported && CrossGeolocator.Current.IsGeolocationEnabled && CrossGeolocator.Current.IsGeolocationAvailable)
+                {
+
+                    var ll = await locator.GetPositionAsync();
+                    Position p = new Position(ll.Latitude, ll.Longitude);
+
+                    Console.WriteLine("vraie pos ");
+
+
+                    return p;
+                }
+                var ll2 = await locator.GetLastKnownLocationAsync();
+                Console.WriteLine("last pos ");
+                return new Position(ll2.Latitude, ll2.Longitude);
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Error localistaion position par default donc ");
+                return defaultPosition;
+            }
+        }
+
     }
 }
